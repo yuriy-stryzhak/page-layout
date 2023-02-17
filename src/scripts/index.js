fe0dab9f198c9798958b67c972e924a8db06a0d8
yuriy.stryzhak <yuriy.stryzhak@gmail.com>
2023-02-17 22:54:53 +0200

    Layouted slider and steps sections

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,35 @@
 import '../styles/index.scss';
 
-document.addEventListener("DOMContentLoaded", function() { 
+// Swiper slider imports
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
+
+
+window.addEventListener("load", function() {
     setTimeout(() => {
         document.querySelector('body').style.opacity = 1;
     }, 500);
 });
+
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: false,
+    slidesPerView: 3,
+    // slidesPerGroup: 3,
+    spaceBetween: 30,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+  console.log(swiper);
