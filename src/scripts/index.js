c16d77d0bd7a8764c0a745209e9ad8f7bef8d776
yuriy.stryzhak <yuriy.stryzhak@gmail.com>
2023-02-22 11:08:55 +0200

    Layouted variations, progress and posts sections

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -12,24 +12,35 @@ window.addEventListener("load", function() {
 });
 
 
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: false,
-    slidesPerView: 3,
-    // slidesPerGroup: 3,
-    spaceBetween: 30,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
+new Swiper('.swiper', {
+  direction: 'horizontal',
+  loop: false,
+  slidesPerView: 3,
+  spaceBetween: 30,
 
-  console.log(swiper);
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+
+// Scroll to Progress section
+const elem = document.querySelector('.progress');
+const progressItems = document.getElementsByClassName('js-progress-item');
+
+document.addEventListener('scroll', function() {
+  const posTop = elem.getBoundingClientRect().top;
+
+  if(posTop + elem.clientHeight <= window.innerHeight && posTop >= 0) {
+    [...progressItems].forEach((el, i) => {
+      setTimeout(() => {
+        el.classList.add('active');
+      }, 500 + (i * 500));
+    });
+  }
+});
